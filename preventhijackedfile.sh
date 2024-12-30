7a21e438017cac2a66238d05423076431399d868
LionHeart <39473760+richcow2018@users.noreply.github.com>
2024-12-30 13:57:33 +0800

    Update preventhijackedfile.sh

--- preventhijackedfile.sh
+++ preventhijackedfile.sh
@@ -7,9 +7,29 @@
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 DATE=`date +"%m_%d_%Y_%H-%M"`
+
 BACKUP_DIR=/home/username/www/foldername/public_html
+FILENAME=/home/username/scripts/filesyncresult.txt
+FILEFOLDER=/home/username/scripts
+FILESIZE=`du -b "${FILENAME}" | cut -f1`
 
 echo -e  "\n==========  $DATE  START ==========" 
+
+echo -e  "\n===  script diff  ====" 
+
+diff_result="$(diff ${FILEFOLDER}/replaceeditedfiles.sh ${FILEFOLDER}/replaceeditedfiles.sh.bak)"
+exit_code="$?"
+echo -e "\n exit code: ${exit_code}" 
+
+if [ ${exit_code} -eq 1 ]
+then
+  echo -e  "\n Script is diff & Rsync !"
+  diff diff ${FILEFOLDER}/replaceeditedfiles.sh ${FILEFOLDER}/replaceeditedfiles.sh.bak
+  rsync -avz ${FILEFOLDER}/replaceeditedfiles.sh.bak ${FILEFOLDER}/replaceeditedfiles.sh
+elif [ ${exit_code} -eq 0 ]
+then
+  echo -e  "\n Script is no diff !" 
+fi
 
 echo -e  "\n===  Class diff  ====" 
 
@@ -20,6 +40,7 @@ echo -e "\n exit code: ${exit_code}"
 if [ ${exit_code} -eq 1 ]
 then
   echo -e  "\n Class is diff & Rsync !" 
+  diff -qr ${BACKUP_DIR}/original/classes ${BACKUP_DIR}/classes
   rsync -avz ${BACKUP_DIR}/original/classes/ ${BACKUP_DIR}/classes/  
 elif [ ${exit_code} -eq 0 ]
 then
@@ -35,6 +56,7 @@ echo -e "\n exit code: ${exit_code}"
 if [ ${exit_code} -eq 1 ]
 then
   echo -e "\n admin diff & Rsync !"
+  diff -qr --exclude=php_errorlog ${BACKUP_DIR}/original/admin ${BACKUP_DIR}/admin
   rsync -avz --exclude 'php_errorlog' ${BACKUP_DIR}/original/admin/ ${BACKUP_DIR}/admin/  
 elif [ ${exit_code} -eq 0 ]
 then
@@ -50,6 +72,7 @@ echo -e "\n exit code: ${exit_code}"
 if [ ${exit_code} -eq 1 ]
 then
   echo -e "\n controllers diff & Rsync !"
+  diff -qr ${BACKUP_DIR}/original/controllers ${BACKUP_DIR}/controllers
   rsync -avz ${BACKUP_DIR}/original/controllers/ ${BACKUP_DIR}/controllers/  
 elif [ ${exit_code} -eq 0 ]
 then
@@ -65,6 +88,7 @@ echo -e "\n exit code: ${exit_code}"
 if [ ${exit_code} -eq 1 ]
 then
   echo -e "\n js diff & Rsync !"
+  diff -qr ${BACKUP_DIR}/original/js ${BACKUP_DIR}/js
   rsync -avz --delete ${BACKUP_DIR}/original/js/ ${BACKUP_DIR}/js/
 elif [ ${exit_code} -eq 0 ]
 then
@@ -80,6 +104,7 @@ echo -e "\n exit code: ${exit_code}"
 if [ ${exit_code} -eq 1 ]
 then
   echo -e "\n override diff & Rsync !"
+  diff -qr ${BACKUP_DIR}/original/override ${BACKUP_DIR}/override
   rsync -avz ${BACKUP_DIR}/original/override/ ${BACKUP_DIR}/override/  
 elif [ ${exit_code} -eq 0 ]
 then
@@ -95,10 +120,20 @@ echo -e "\n exit code: ${exit_code}"
 if [ ${exit_code} -eq 1 ]
 then
   echo -e "\n bin diff & Rsync !"
+  diff -qr ${BACKUP_DIR}/original/bin ${BACKUP_DIR}/bin
   rsync -avz ${BACKUP_DIR}/original/bin/ ${BACKUP_DIR}/bin/  
 elif [ ${exit_code} -eq 0 ]
 then
   echo -e "\n bin no diff !"
+fi
+
+echo -e  "\n===  housekeeping  ====" 
+
+if [ ${FILESIZE} -gt 52428800 ]
+then
+  echo -e "\n compress and truncate the file !"
+  zip ${FILEFOLDER}/filesyncresult_$(date +%F-%H).zip ${FILENAME}
+  cat /dev/null >  ${FILENAME} 
 fi
 
 echo -e  "==========  $DATE  END ==========\n"
